e00b0eca8c63e62f74dac0c21b24bf599dc407ec
wjampa@gmail.com <wjampa@gmail.com@0516cfbf-63a6-0c3f-9afe-060a87b9371f>
2012-05-09 20:31:20 +0000

    Base do projeto com as libs do flerry para o java e para o air, e o executavel para o windows localizar o java.

---
+++ POD-RMI-AIR/src-java/br/com/hotel/Teste.java
@@ -0,0 +1,17 @@
+package br.com.hotel;
+
+public class Teste {
+
+	public Teste() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
